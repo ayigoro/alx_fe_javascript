c967c2126fe1d5b79595f334daa5aaa61359fcc7
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-11-01 14:22:43 +0100

    Added createAddQuoteForm and addQuote files

---
+++ dom-manipulation/script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const quotes = [
+  { text: "The only way to do great work is to love what you do.", category: "Inspiration" },
+  { text: "Life is what happens when you're busy making other plans.", category: "Life" },
+  { text: "Strive not to be a success, but rather to be of value.", category: "Motivation" },
+  { text: "Get busy living or get busy dying.", category: "Life" },
+  { text: "The mind is everything. What you think you become.", category: "Wisdom" }
+]
+ 
+newQuote.addEventListener('click',showRandomQuote)
+
+function showRandomQuote (){
+const newQuote = document.getElementById('newQuote')
+const quoteDisplay = document.getElementById('quoteDisplay')
+const randomIndex = Math.floor(Math.random() * quotes.length)
+const randomQuote = quotes[randomIndex]
+if (quoteDisplay){
+   quoteDisplay.innerHTML = `<p>Quote: "${randomQuote.text}"</p><em> Category: ${randomQuote.category}</em>`;
+}
+}
+function createAddQuoteForm(){
+      const newQuoteText = document.getElementById('newQuoteText')
+      const newQuoteCategory = document.getElementById('newQuoteCategory')
+      if (newQuoteText && newQuoteCategory){
+        const newQuote = {
+          text:newQuoteText,
+          category:newQuoteCategory
+        }
+        quotes.push(newQuote)
+      }
+      else{
+        console.error('Both text and category are required to add a quote')
+      }
+    }
+
+    function addQuote (){
+      createAddQuoteForm()
+    }
+    addQuote()
+})
