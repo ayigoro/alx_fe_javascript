d0e413b7b97f0468db268ad1544e53ff98001c9b
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-11-01 21:17:03 +0100

    Added input for file input

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -56,5 +56,16 @@ function createAddQuoteForm(){
       createAddQuoteForm()
     }
     addQuote()
+
+    function importFromJsonFile(event) {
+      const fileReader = new FileReader();
+      fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+      fileReader.readAsText(event.target.files[0]);
+  }
 })
 // ["createElement", "appendChild"]
